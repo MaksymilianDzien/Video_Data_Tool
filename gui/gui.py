3c6e71b83209c27b61ccad5b3daa0389c3b258f5
JA <dzmaks252@gmail.com>
2026-05-03 22:43:08 +0200

    fixed margins between panels

--- gui/gui.py
+++ gui/gui.py
@@ -2,45 +2,80 @@ from PyQt5.QtWidgets import (
     QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QFrame
 )
 
+
 class Main_Gui(QMainWindow):
+
     def __init__(self):
-        super().__init__()
+        super(Main_Gui, self).__init__()
 
         # Title and size gui
-        self.setWindowTitle("Simpe GUI")
+        self.setWindowTitle("Simple GUI")
         self.resize(1200, 800)
+        self.Create_GUI()
+
+    def Create_GUI(self):
 
         # main widget
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
+        main_widget = QWidget()
+        self.setCentralWidget(main_widget)
 
         # main layout
-        main_layout = QVBoxLayout()
-        central_widget.setLayout(main_layout)
+        main_gui_layout = QVBoxLayout()
+        main_gui_layout.setContentsMargins(0, 0, 0, 0)
+        main_gui_layout.setSpacing(0)
+        main_widget.setLayout(main_gui_layout)
 
-        # top_panel
-        self.top_panel = QFrame()
-        self.top_panel.setFixedHeight(60)
-        self.top_panel.setStyleSheet("background-color: #2c3e50;")
-        main_layout.addWidget(self.top_panel)
+        # Create all panels
+        main_gui_layout.addWidget(self.create_top_panel())
+        main_gui_layout.addLayout(self.create_gui_main_content())
+        main_gui_layout.addWidget(self.create_bottom_panel())
 
-        # main_gui_layout
+    # top_panel
+    def create_top_panel(self):
+        top_panel = QFrame()
+        top_panel.setFixedHeight(60)
+        top_panel.setStyleSheet("background-color: #2c3e50;")
+
+        return top_panel
+
+    # main_gui_layout
+    def create_gui_main_content(self):
         content_layout = QHBoxLayout()
-        main_layout.addLayout(content_layout)
+        content_layout.setContentsMargins(0, 0, 0, 0)
+        content_layout.setSpacing(0)
 
-        # left_panel
-        self.left_panel = QFrame()
-        self.left_panel.setFixedWidth(80)
-        self.left_panel.setStyleSheet("background-color: #e74c3c;")
-        content_layout.addWidget(self.left_panel)
+        content_layout.addWidget(self.create_left_panel())
+        content_layout.addWidget(self.create_middle_panel())
+        content_layout.addWidget(self.create_right_panel())
 
-        # middle_panel
-        self.middle_panel = QFrame()
-        self.middle_panel.setStyleSheet("background-color: #1abc9c;")
-        content_layout.addWidget(self.middle_panel)
+        return content_layout
 
-        # right_panel
-        self.right_panel = QFrame()
-        self.right_panel.setFixedWidth(300)
-        self.right_panel.setStyleSheet("background-color: #f1c40f;")
-        content_layout.addWidget(self.right_panel)
+    # left_panel
+    def create_left_panel(self):
+        left_panel = QFrame()
+        left_panel.setFixedWidth(80)
+        left_panel.setStyleSheet("background-color: #e74c3c;")
+
+        return left_panel
+
+    # middle_panel
+    def create_middle_panel(self):
+        middle_panel = QFrame()
+        middle_panel.setStyleSheet("background-color: #1abc9c;")
+
+        return middle_panel
+
+    # right_panel
+    def create_right_panel(self):
+        right_panel = QFrame()
+        right_panel.setFixedWidth(300)
+        right_panel.setStyleSheet("background-color: #f1c40f;")
+
+        return right_panel
+    # bottom_panel
+    def create_bottom_panel(self):
+        bottom_bar = QFrame()
+        bottom_bar.setFixedHeight(80)
+        bottom_bar.setStyleSheet("background-color: #244eff;")
+
+        return bottom_bar
