4ccafdde0e17c28e4feb3478e1bd1d269a612701
JA <dzmaks252@gmail.com>
2026-08-28 15:35:01 +0200

    add slider button fuckiton

--- gui/gui.py
+++ gui/gui.py
@@ -99,6 +99,7 @@ class Main_Gui(QMainWindow):
             top_mid_button.setStyleSheet(button_style)
 
 
+
             match i:
                 #set value to start position
                 case 4:
